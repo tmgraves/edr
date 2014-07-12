80edf1205930a29dc3f999ccc34675576677dc7f
tmgraves <tadashigraves@gmail.com>
2014-07-12 14:02:53 -0700

    add event fields

--- EDR/Models/Event.cs
+++ EDR/Models/Event.cs
@@ -18,5 +18,24 @@ namespace EDR.Models
 
         [Display(Name = "Open for Registration")]
         public bool IsAvailable { get; set; }
+
+        public System.DateTime StartDate { get; set; }
+        public System.DateTime EndDate { get; set; }
+        public Nullable<System.TimeSpan> StartTime { get; set; }
+        public Nullable<System.TimeSpan> EndTime { get; set; }
+        public bool AllDay { get; set; }
+        public string Link { get; set; }
+        public Nullable<decimal> Cost { get; set; }
+        public string LocationType { get; set; }
+        public string Address { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public Nullable<int> ParentEventID { get; set; }
+        public Nullable<int> Frequency { get; set; }
+        public string DaysOfWeek { get; set; }
+        public Nullable<int> GroupID { get; set; }
     }
 }
