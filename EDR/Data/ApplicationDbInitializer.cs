8e8ce1c23c11059abb742605806db1b28779a865
Jesse Johnson <johnson.jesse@live.com>
2014-07-13 15:06:19 -0700

    Added  DbInitializer

---
+++ EDR/Data/ApplicationDbInitializer.cs
@@ -0,0 +1,17 @@
+﻿using EDR.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EDR.Data
+{
+    public class ApplicationDbInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            base.Seed(context);
+        }
+    }
+}
--- EDR/Global.asax.cs
+++ EDR/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using EDR.Data;
+using EDR.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +15,7 @@ namespace EDR
     {
         protected void Application_Start()
         {
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
