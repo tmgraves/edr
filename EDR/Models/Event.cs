69b2ef3ea59a658faeaa47c1b3ffca81f6797a45
Jesse Johnson <johnson.jesse@live.com>
2014-07-13 16:38:12 -0700

    Model cleanup

--- EDR/Models/Event.cs
+++ EDR/Models/Event.cs
@@ -13,26 +13,13 @@ namespace EDR.Models
 
         [Required]
         public string Summary { get; set; }
-
         public string Description { get; set; }
-
-        [Display(Name = "Open for Registration")]
         public bool IsAvailable { get; set; }
-
-        public System.DateTime StartDate { get; set; }
-        public System.DateTime EndDate { get; set; }
-        public Nullable<System.TimeSpan> StartTime { get; set; }
-        public Nullable<System.TimeSpan> EndTime { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public bool AllDay { get; set; }
         public string Link { get; set; }
         public Nullable<decimal> Cost { get; set; }
-        public string LocationType { get; set; }
-        public string Address { get; set; }
-        public string Address2 { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Zip { get; set; }
-        public string Country { get; set; }
         public Nullable<int> ParentEventID { get; set; }
         public Nullable<int> Frequency { get; set; }
         public string DaysOfWeek { get; set; }
