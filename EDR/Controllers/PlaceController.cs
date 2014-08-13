b47487429d688ce8c38015984e3c2796199604d0
tmgraves <tadashigraves@gmail.com>
2014-08-12 19:59:32 -0700

    teacher details

--- EDR/Controllers/PlaceController.cs
+++ EDR/Controllers/PlaceController.cs
@@ -11,21 +11,48 @@ namespace EDR.Controllers
 {
     public class PlaceController : BaseController
     {
-        public ActionResult Details(int id)
+        public ActionResult Details(int id, int? danceStyle, string teacher, int? skillLevel)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
+            var TeacherLst = DataContext.IdentityUsers.OfType<Teacher>().Where(x => x.Classes.Any(c => c.Place.Id == id) || x.Workshops.Any(w => w.Place.Id == id));
+            ViewBag.teacher = new SelectList(TeacherLst, "Id", "FullName", teacher);
+
+            var DanceStyleLst = DataContext.DanceStyles.ToList();
+            ViewBag.danceStyle = new SelectList(DanceStyleLst, "Id", "Name", danceStyle);
+
+            var SkillLevelLst = new List<int> { 1, 2, 3, 4, 5 };
+            ViewBag.skillLevel = new SelectList(SkillLevelLst);
+
             var viewModel = new PlaceDetailViewModel();
             viewModel.Place = DataContext.Places.Find(id);
             viewModel.Classes = DataContext.Events.Include("Teachers").Include("DanceStyles").Include("Users").OfType<Class>().Where(c => c.Place.Id == id).Where(x => x.IsAvailable == true).Where(y => y.StartDate >= DateTime.Now).OrderBy(z => z.StartDate).ToList().Take(5);
-            viewModel.Socials = DataContext.Events.Include("DanceStyles").Include("Users").OfType<Social>().Where(c => c.Place.Id == id).Where(x => x.IsAvailable == true).Where(y => y.StartDate >= DateTime.Now).OrderBy(z => z.StartDate).ToList().Take(5);
-            viewModel.Concerts = DataContext.Events.Include("DanceStyles").Include("Users").OfType<Concert>().Where(c => c.Place.Id == id).Where(x => x.IsAvailable == true).Where(y => y.StartDate >= DateTime.Now).OrderBy(z => z.StartDate).ToList().Take(5);
-            viewModel.Conferences = DataContext.Events.Include("DanceStyles").Include("Users").OfType<Conference>().Where(c => c.Place.Id == id).Where(x => x.IsAvailable == true).Where(y => y.StartDate >= DateTime.Now).OrderBy(z => z.StartDate).ToList().Take(5);
-            viewModel.Parties = DataContext.Events.Include("DanceStyles").Include("Users").OfType<Party>().Where(c => c.Place.Id == id).Where(x => x.IsAvailable == true).Where(y => y.StartDate >= DateTime.Now).OrderBy(z => z.StartDate).ToList().Take(5);
-            viewModel.OpenHouses = DataContext.Events.Include("DanceStyles").Include("Users").OfType<OpenHouse>().Where(c => c.Place.Id == id).Where(x => x.IsAvailable == true).Where(y => y.StartDate >= DateTime.Now).OrderBy(z => z.StartDate).ToList().Take(5);
+            var Events = DataContext.Events.Include("DanceStyles").Include("Users").Where(c => c.Place.Id == id).Where(x => x.IsAvailable == true).Where(y => y.StartDate >= DateTime.Now).OrderBy(z => z.StartDate).ToList().Take(5);
+
+            if (danceStyle != null)
+            {
+                viewModel.Classes = viewModel.Classes.Where(x => x.DanceStyles.Any(s => s.Id == danceStyle));
+                Events = Events.Where(x => x.DanceStyles.Any(s => s.Id == danceStyle));
+            }
+
+            if (teacher != null && teacher != "")
+            {
+                viewModel.Classes = viewModel.Classes.Where(x => x.Teachers.Any(t => t.Id == teacher));
+            }
+
+            if (skillLevel != null)
+            {
+                viewModel.Classes = viewModel.Classes.Where(x => x.SkillLevel == skillLevel);
+            }
+
+            viewModel.Socials = Events.OfType<Social>();
+            viewModel.Concerts = Events.OfType<Concert>();
+            viewModel.Conferences = Events.OfType<Conference>();
+            viewModel.Parties = Events.OfType<Party>();
+            viewModel.OpenHouses = Events.OfType<OpenHouse>();
 
             return View(viewModel);
         }
