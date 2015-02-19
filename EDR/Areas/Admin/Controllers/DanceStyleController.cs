a2a08c824db96dde9896a300b7d64cf9cda91a20
tmgraves <tadashigraves@gmail.com>
2015-02-19 13:40:38 -0800

    pp

---
+++ EDR/Areas/Admin/Controllers/DanceStyleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EDR.Controllers;
+using System.Data.Entity;
+using EDR.Utilities;
+using System.IO;
+
+namespace EDR.Areas.Admin.Controllers
+{
+    public class DanceStyleController : BaseController
+    {
+        // GET: Admin/DanceStyle
+        public ActionResult Index()
+        {
+            var styles = DataContext.DanceStyles;
+            return View(styles);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var style = DataContext.DanceStyles.Where(s => s.Id == id).Include("Videos").FirstOrDefault();
+            return View(style);
+        }
+    }
+}
