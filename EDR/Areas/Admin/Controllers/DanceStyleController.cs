ae506b77d559aa0fca7a9b5219d9ec05b1856fe2
tmgraves <tadashigraves@gmail.com>
2015-02-19 18:08:46 -0800

    dance style pictures

--- EDR/Areas/Admin/Controllers/DanceStyleController.cs
+++ EDR/Areas/Admin/Controllers/DanceStyleController.cs
@@ -7,22 +7,44 @@ using EDR.Controllers;
 using System.Data.Entity;
 using EDR.Utilities;
 using System.IO;
+using EDR.Models;
 
 namespace EDR.Areas.Admin.Controllers
 {
     public class DanceStyleController : BaseController
     {
         // GET: Admin/DanceStyle
+        [Authorize(Roles = "Admin")]
         public ActionResult Index()
         {
             var styles = DataContext.DanceStyles;
             return View(styles);
         }
 
+        [Authorize(Roles = "Admin")]
         public ActionResult Details(int id)
         {
             var style = DataContext.DanceStyles.Where(s => s.Id == id).Include("Videos").FirstOrDefault();
             return View(style);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult UploadPicture(HttpPostedFileBase file, int id)
+        {
+            UploadFile newFile = ApplicationUtility.LoadPicture(file);
+
+            if (newFile.UploadStatus == "Success")
+            {
+                DataContext.DanceStyles.Where(s => s.Id == id).FirstOrDefault().PhotoUrl = newFile.FilePath;
+                DataContext.SaveChanges();
+                return RedirectToAction("Details", "DanceStyle", new { id = id } );
+            }
+            else
+            {
+                ViewBag.Message = newFile.UploadStatus;
+                return RedirectToAction("Details", "DanceStyle", new { id = id });
+            }
         }
     }
 }
--- EDR/Models/Picture.cs
+++ EDR/Models/Picture.cs
@@ -7,7 +7,7 @@ using System.Web;
 
 namespace EDR.Models
 {
-    public abstract class Picture : Entity
+    public class Picture : Entity
     {
         public string Title { get; set; }
         public string Filename { get; set; }
